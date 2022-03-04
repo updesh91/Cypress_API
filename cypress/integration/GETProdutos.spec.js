b05e6dd066c8ee32f31ae5f998e46bfe795dd09a
Updesh  Kumar <updesh.kumar@Updesh-Kumar.local>
2022-03-04 14:45:30 +0530

    [API_Test_using_Cypress]

---
+++ cypress/integration/GETProdutos.spec.js
@@ -0,0 +1,15 @@
+/// <reference types="cypress" />
+
+describe('Products api', () => {
+    context('GET /produtos', () => {
+        it('should return a list with all products', () => {
+            cy.request({
+                method: 'GET',
+                url: 'https://serverest.dev/produtos'
+            })
+                .should((response) => {
+                    cy.log(JSON.stringify(response.body))
+                });
+        });
+    });
+});
